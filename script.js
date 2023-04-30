1242bac58ddb6d65fdcbaf7bc544de2dc708be71
Dushyant Jangid <107930946+Dushyant1122@users.noreply.github.com>
2023-04-30 23:08:24 +0530

    updated

--- script.js
+++ script.js
@@ -1,35 +1,10 @@
-// var index = 0;
-// show_image(index);
-
-// function show_image(i){
-//     index += i;
-
-//     var images = document.getElementsByClassName("image");
-//     var dots = document.getElementsByClassName("dot");
-
-//     for(i = 0; i < images.length; i++)
-//         images[i].style.display = "none";
-
-//     for(i = 0; i < dots.length; i++)
-//         dots[i].className = dots[i].className.replace("active", " ");
-
-//        if(index > images.length - 1)
-//        index = 0;
-
-//        if (index < 0)
-//        index = images.length - 1;
-
-//        images[index].style.display = "block";
-//        dots[index].className = "active";
-// }
-
 const sliderContainer = document.querySelector(".slider-container");
 const prevBtn = document.querySelector(".prev-btn");
 const nextBtn = document.querySelector(".next-btn");
 const sliderDots = document.querySelector(".slider-dots");
 
 const images = [
-  "https://picsum.photos/600/800",
+  "pic-1.jpg",
   "pic-2.jpg",
   "pic-3.jpg",
   "pic-4.jpg",
